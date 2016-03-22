74f216225b1a0a35a0be77259e10093745b29a27
MJG <MJG@MacBook-Pro.local>
2016-03-21 21:02:39 -0400

    commiting good version before I try something crazy

--- js/dice.js
+++ js/dice.js
@@ -1,12 +1,14 @@
+var starttime = new Date()
 var leftdie = document.getElementById('leftdienumber');
 var rightdie = document.getElementById('rightdienumber');
 var rollbutton = document.getElementById('rollbutton');
 var footerdate = document.getElementById('footerdate');
 var title = document.getElementById('title');
+var winnerstats = document.getElementById('winnerstats');
 
 var dicegame = {
-  starttime: new Date(),
-  currentround: 1,
+  starttime: starttime.getTime(),
+  currentround: 0,
   history: [],
   roll: function roll(){
     leftdienumber.innerHTML = Math.floor((Math.random() * 6)) + 1;
@@ -14,18 +16,38 @@ var dicegame = {
   }
 }
 
-rollbutton.addEventListener('click', function(){
-  dicegame.roll();
-  if((Number(leftdienumber.innerHTML) + Number(rightdienumber.innerHTML) !== 7) ||
-      (Number(leftdienumber.innerHTML) + Number(rightdienumber.innerHTML) !== 11)) {
-        title.innerHTML = "Try Again";
-      }
-  if((Number(leftdienumber.innerHTML) + Number(rightdienumber.innerHTML) === 7) ||
-      (Number(leftdienumber.innerHTML) + Number(rightdienumber.innerHTML) === 11)) {
-        title.innerHTML = "Winner!";
-      } 
+rollbutton.addEventListener('mousedown', function(){
+  rollbutton.style.color = "white";
+  rollbutton.style["background-color"] = "black";
 })
 
-console.log(title.innerHTML);
+rollbutton.addEventListener('mouseup', function(){
+  rollbutton.style.color = "black";
+  rollbutton.style["background-color"] = "white";
 
-footerdate.innerHTML = new Date();
+})
+
+rollbutton.addEventListener('click', function(){
+  dicegame.roll();
+  var sum = Number(leftdienumber.innerHTML) + Number(rightdienumber.innerHTML);
+  if((sum !== 7) ||
+      (sum !== 11)) {
+        title.innerHTML = "Try Again";
+        winnerstats.innerHTML = "";
+        dicegame.currentround = dicegame.currentround + 1;
+      }
+  if((sum === 7) ||
+      (sum === 11)) {
+        dicegame.history.push({
+          rounds: dicegame.currentround,
+          time: ((new Date().getTime() - starttime)/1000)
+        })
+        console.log(dicegame.history);
+        title.innerHTML = "Winner!";
+        winnerstats.innerHTML = "(It took you " + dicegame.currentround + " tries " + "and " + ((new Date().getTime() - starttime)/1000) + " seconds)"
+        dicegame.currentround = 0;
+        starttime = new Date().getTime();
+      };
+})
+
+footerdate.innerHTML = "Game Started " + starttime.getFullYear() + "-" + starttime.getMonth() + "-" + starttime.getDate() + " at " + starttime.getHours() + ":" + starttime.getMinutes()
